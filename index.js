82f9d8be77bd2d1546fd5f026aeff2b0195d0718
Can Noah <87754848+Asareff@users.noreply.github.com>
2024-01-13 19:52:50 +0100

    2.10 commits

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cors({
 }));
 
 // Connect to MongoDB using Mongoose
-mongoose.connect('mongodb://localhost:27017/myDatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://cannoah:NKcJpeB1M6jcUKix@cluster0.njmial8.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Import Mongoose models
 const { Movie, User } = require('./models');

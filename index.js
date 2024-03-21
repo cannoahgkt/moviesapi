5ee144be53e308fb4409202aeecab0a6f7ee597a
Can Noah <87754848+Asareff@users.noreply.github.com>
2024-03-21 21:17:13 +0100

    cors

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ app.listen(port, '0.0.0.0', () => {
 app.use(bodyParser.json());
 
 // Use Cross-Origin Resource Sharing
-const cors = require("cors");
-app.use(cors());
+/*const cors = require("cors");
+app.use(cors());*/
 
 // Creates a list of allowed domains
-/*let allowedOrigins = ['http://127.0.0.1:3000', 'http://testsite.com', 'https://myflixonline.netlify.app'];
+let allowedOrigins = ['http://127.0.0.1:3000', 'http://testsite.com', 'https://myflixonline.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
@@ -31,7 +31,7 @@ app.use(cors({
     }
     return callback(null, true);
   }
-}));*/
+}));
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
